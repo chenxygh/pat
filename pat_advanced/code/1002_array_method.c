9d9ccd4734dd772e9ca6324cf4edc641dacb942f
cxy <37500268+chenxygh@users.noreply.github.com>
2019-03-15 12:32:25 +0800

    修复 array_method 的错误

--- pat_advanced/code/1002_array_method.c
+++ pat_advanced/code/1002_array_method.c
@@ -2,7 +2,7 @@
 * @Author: cxy
 * @Date:   2019-02-26 11:28:46
 * @Last Modified by:   cxy
-* @Last Modified time: 2019-03-15 12:08:26
+* @Last Modified time: 2019-03-15 12:31:17
 */
 #include <stdio.h>
 
@@ -25,16 +25,23 @@ int main(int argc, char const *argv[])
 	getPoly(expo);
 
 	// 1. 再遍历全部
-	// int cnt = 0;
-	// for (int i = 1000; i >= 0; --i) if (expo[i] != 0) cnt++;
-	// printf("%d", cnt);
-	// for (int i = 1000; i >= 0; --i) if (expo[i] != 0) printf(" %d %.1lf", i, expo[i]);
+	int cnt = 0;
+	for (int i = 1000; i >= 0; --i) if (expo[i] != 0) cnt++;
+	printf("%d", cnt);
+	for (int i = 1000; i >= 0; --i) if (expo[i] != 0) printf(" %d %.1lf", i, expo[i]);
 
-	// 2. 在第一次统计之时，则前移 result
-	int length = 0;
-	for (int i = 0; i <= 1000; expo[i] != 0? expo[length++] = expo[i++]: ++i);
-	printf("%d", length);
-	while (length--) printf(" %d %.1lf", length, expo[length]);
+
+
+	// 2. 在第一次统计之时，则前移 result                ------> 错误！！！
+	/**
+	 * 自作聪明！！！ 数组法的本质就是把指数和数组的下标联系起来！！！
+	 * 如果动了数组元素的位置，那么这个方法就完全失效了！
+	 */
+	// 大错特错！！！
+	// int length = 0;
+	// for (int i = 0; i <= 1000; expo[i] != 0? expo[length++] = expo[i++]: ++i);
+	// printf("%d", length);
+	// while (length--) printf(" %d %.1lf", length, expo[length]);
 
 	return 0;
 }
--- pat_advanced/code/test.c
+++ pat_advanced/code/test.c
@@ -2,18 +2,16 @@
 * @Author: cxy
 * @Date:   2019-02-25 20:53:17
 * @Last Modified by:   cxy
-* @Last Modified time: 2019-03-01 14:12:35
+* @Last Modified time: 2019-03-15 12:17:07
 */
 #include <stdio.h>
 #include <string.h>
 
 int main(int argc, char const *argv[])
 {
-	int a = 0;
-	int b = 10;
-
-	if (a ||(b = 99));
-	printf("%d", a && (b = 99));
-	// printf("%d, %d\n", a, b);
+	int arr[] = {0, 1, 2, 0, 3, 4, 5, 6, 7, 8, 9, 0, 11, 12, 13};
+	int length = 0;
+	for (int i = 0; i < 15; arr[i] != 0? arr[length++] = arr[i++]: ++i);
+	while (length--) printf("%d\n", arr[length]);
 	return 0;
 }

7e69c5e2e074bc158d9e683d735f08ed635cd8d3
cxy <37500268+chenxygh@users.noreply.github.com>
2019-03-21 22:02:20 +0800

    1027 Colors in Mars 13进制的转换

--- pat_advanced/code/test.c
+++ pat_advanced/code/test.c
@@ -2,7 +2,7 @@
 * @Author: cxy
 * @Date:   2019-02-25 20:53:17
 * @Last Modified by:   cxy
-* @Last Modified time: 2019-03-21 20:44:19
+* @Last Modified time: 2019-03-21 21:53:52
 */
 #include <stdio.h>
 #include <string.h>
@@ -11,14 +11,12 @@ int a;
 
 int main(int argc, char const *argv[])
 {
-	char num[101] = {};
-	char *str = num;
-
-	scanf("%s", num);
-	while (*str != '\0') {
-		printf("%c\n", *(str++));
-	}
-	printf("%s\n", num);
+	const char marsColor[][2] = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C"};
+	char res[8] = {};
+	res[0] = '#';
+	int decimalColor = 56;
+	strcat(res, marsColor[decimalColor / 13]);
+	printf("%s\n", res);
 
 	return 0;
 }

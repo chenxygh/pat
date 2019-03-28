540254870c047ddb55a891b015a838c1d0bf28ba
cxy <37500268+chenxygh@users.noreply.github.com>
2019-03-28 17:28:44 +0800

    realloc 必须配合 malloc 或者 calloc 或者 realloc 之下使用! 数组都不行!

--- pat_advanced/code/test.c
+++ pat_advanced/code/test.c
@@ -2,44 +2,53 @@
 * @Author: cxy
 * @Date:   2019-02-25 20:53:17
 * @Last Modified by:   cxy
-* @Last Modified time: 2019-03-28 16:20:41
+* @Last Modified time: 2019-03-28 17:25:39
 */
 #include <stdio.h>
 #include <stdlib.h>
 
+int* plusOne(int* digits, int digitsSize, int* returnSize) {
+    int cin = 0;
+    int add = 1;
+    int n = digitsSize;
+    while (n--) {
+        int sum = digits[n] + cin + add;
+        digits[n] = sum % 10;
+        cin = sum / 10;
+        if (add) add = 0;
+    }
+    *returnSize = digitsSize;
+    if (cin) {
+    	*returnSize = digitsSize + 1;
+        int newLen = *returnSize;
+        printf("newLen: %d\n", newLen);
+        int *p = (int *)realloc(digits, newLen * sizeof(int));
+        while (!p) p = (int *)realloc(digits, newLen * sizeof(int));
+        printf("p's address: %p\n", p);
+        printf("digits's address: %p\n", digits);
+        // TODO: insert into new-digits's head
+        // while (--newLen) p[newLen] = p[newLen - 1];
+        // p[0] = cin;
+        // for (int i = 0; i < newLen; ++i) printf("%d\n", p[i]);
+        // exit(0);
+        // return p;
+    }
+    
+    return digits;
+}
+
 int main(int argc, char const *argv[])
 {
-	int nums[] = {0, 2, 1, 5, 6, 7, 3, 4, 9, 8};
-	int length = 10;
-	for (int i = 0; i < length - 1; ++i) {
-		char hasChange = 0;
-		for (int j = length - 1; j >= i + 1; --j) {
-			if (nums[j] < nums[j - 1]) {
-				int temp = nums[j];
-				nums[j] = nums[j - 1];
-				nums[j - 1] = temp;
-				hasChange = 1;
-			}
-		}
-		if (!hasChange) break;
-	}
-
-	for (int i = 0; i < length; ++i) printf("%d\n", nums[i]);
-	printf("\n");
-	
-	length = 12;
-	// realloc 要使用一个新的指针来接收新内存的地址！ 因为可能不是 append, 而是 new space
-	int *p = (int *)realloc(nums, length * sizeof(int));
-	for (int i = 0; i < length; ++i) printf("%d\n", nums[i]);
-	printf("\n");
-
-	int a = 10;
-	int b = 1;
-	int c = 0;
-	c = b = a;
-	printf("%d\n", a);
-	printf("%d\n", b);
-	printf("%d\n", c);
+	int *digits = (int *)malloc(3 * sizeof(int));
+	digits[0] = 0;
+	digits[1] = 1;
+	digits[2] = 2;
+	// int digits[] = {9, 9, 9};
+	int digitsSize = 3;
+	int returnSize = 20;
+	// int *p = plusOne(digits, digitsSize, &returnSize);
+	int *p = (int *)realloc(digits, 20 * sizeof(int));
+	for (int i = 0; i < returnSize; ++i) printf("%d\n", p[i]);
 
 	return 0;
 }

f1a7a3965c1ca938aeef557ae27c306aebe7a4c1
cxy <37500268+chenxygh@users.noreply.github.com>
2019-03-28 16:21:34 +0800

    注意 realloc 要使用新指针来接受新内存的地址！

--- pat_advanced/code/test.c
+++ pat_advanced/code/test.c
@@ -2,28 +2,44 @@
 * @Author: cxy
 * @Date:   2019-02-25 20:53:17
 * @Last Modified by:   cxy
-* @Last Modified time: 2019-03-23 15:06:32
+* @Last Modified time: 2019-03-28 16:20:41
 */
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char const *argv[])
 {
-	int num[] = {0, 2, 1, 5, 6, 7, 3, 4, 9, 8};
+	int nums[] = {0, 2, 1, 5, 6, 7, 3, 4, 9, 8};
 	int length = 10;
 	for (int i = 0; i < length - 1; ++i) {
-		char isChange = 0;
+		char hasChange = 0;
 		for (int j = length - 1; j >= i + 1; --j) {
-			if (num[j] < num[j - 1]) {
-				int temp = num[j];
-				num[j] = num[j - 1];
-				num[j - 1] = temp;
-				isChange = 1;
+			if (nums[j] < nums[j - 1]) {
+				int temp = nums[j];
+				nums[j] = nums[j - 1];
+				nums[j - 1] = temp;
+				hasChange = 1;
 			}
 		}
-		if (!isChange) break;
+		if (!hasChange) break;
 	}
 
-	for (int i = 0; i < length; ++i) printf("%d\n", num[i]);
+	for (int i = 0; i < length; ++i) printf("%d\n", nums[i]);
+	printf("\n");
+	
+	length = 12;
+	// realloc 要使用一个新的指针来接收新内存的地址！ 因为可能不是 append, 而是 new space
+	int *p = (int *)realloc(nums, length * sizeof(int));
+	for (int i = 0; i < length; ++i) printf("%d\n", nums[i]);
+	printf("\n");
+
+	int a = 10;
+	int b = 1;
+	int c = 0;
+	c = b = a;
+	printf("%d\n", a);
+	printf("%d\n", b);
+	printf("%d\n", c);
 
 	return 0;
 }

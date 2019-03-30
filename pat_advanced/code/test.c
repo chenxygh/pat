244b12a70d50fdb2254218c46657e3dedfc50f0d
cxy <37500268+chenxygh@users.noreply.github.com>
2019-03-30 21:23:25 +0800

    memset 的使用

--- pat_advanced/code/test.c
+++ pat_advanced/code/test.c
@@ -2,7 +2,7 @@
 * @Author: cxy
 * @Date:   2019-02-25 20:53:17
 * @Last Modified by:   cxy
-* @Last Modified time: 2019-03-30 21:19:20
+* @Last Modified time: 2019-03-30 21:23:02
 */
 #include <stdio.h>
 #include <memory.h>
@@ -16,6 +16,16 @@ typedef struct {
 
 int main(int argc, char const *argv[])
 {
+	/**
+	 * void *memset(void *s, int ch, size_t n);
+	 * memset: 头文件: memory 或者 string
+	 * 注意，ch 只取后八位，每一次也是赋值 1 BYTE
+	 * 所以，一般的，对 int 型，ch 为 0 或者 1 这种，后八位一致的，会有好的重置效果
+	 * 当然，对于字符数组，无所谓了
+	 * 小心字符常量，是无法修改的
+	 *
+	 * 总之，记住，按字节重置
+	 */
 	MSS thisSum = {1, 2, 3, {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}};
 	printf("%d\n", thisSum.start);
 	printf("%d\n", thisSum.end);

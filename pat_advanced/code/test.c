92384e8a5127cfc1b4d9968cea37402cafaf0780
cxy <37500268+chenxygh@users.noreply.github.com>
2019-03-30 20:33:09 +0800

    daily update

--- pat_advanced/code/test.c
+++ pat_advanced/code/test.c
@@ -2,53 +2,55 @@
 * @Author: cxy
 * @Date:   2019-02-25 20:53:17
 * @Last Modified by:   cxy
-* @Last Modified time: 2019-03-28 17:25:39
+* @Last Modified time: 2019-03-29 23:55:19
 */
 #include <stdio.h>
 #include <stdlib.h>
 
-int* plusOne(int* digits, int digitsSize, int* returnSize) {
-    int cin = 0;
-    int add = 1;
-    int n = digitsSize;
-    while (n--) {
-        int sum = digits[n] + cin + add;
-        digits[n] = sum % 10;
-        cin = sum / 10;
-        if (add) add = 0;
-    }
-    *returnSize = digitsSize;
-    if (cin) {
-    	*returnSize = digitsSize + 1;
-        int newLen = *returnSize;
-        printf("newLen: %d\n", newLen);
-        int *p = (int *)realloc(digits, newLen * sizeof(int));
-        while (!p) p = (int *)realloc(digits, newLen * sizeof(int));
-        printf("p's address: %p\n", p);
-        printf("digits's address: %p\n", digits);
-        // TODO: insert into new-digits's head
-        // while (--newLen) p[newLen] = p[newLen - 1];
-        // p[0] = cin;
-        // for (int i = 0; i < newLen; ++i) printf("%d\n", p[i]);
-        // exit(0);
-        // return p;
+typedef struct LNode {
+    char val;
+    struct LNode *next;
+    struct LNode *pre;
+} LNode;
+
+bool isValid(char* s) {
+    struct LNode *head = (struct LNode *)malloc(sizeof(struct LNode));
+    head->next = NULL;
+    head->pre = NULL;
+    head->val = -1;
+    struct LNode *top = head;
+    
+    while (s[0] != '\0') {
+        if (s[0] == '(' || s[0] == '{' || s[0] == '[') {
+            // push
+            struct LNode *temp = (struct LNode *)malloc(sizeof(struct LNode));
+            temp->val = s[0];
+            temp->next = NULL;
+            temp->pre = top;
+            top->next = temp;
+            top = temp;
+            ++s;
+            continue;
+        }
+        
+        char right = top->val == '('? ')': top->val == '{'? '}': ']';
+        if (right != s[0]) return false;
+        
+        // pop
+        struct LNode *temp = top;
+        top = top->pre;
+        top->next = NULL;
+        free(temp);
+        ++s;
     }
     
-    return digits;
+    return top == head;
 }
 
 int main(int argc, char const *argv[])
 {
-	int *digits = (int *)malloc(3 * sizeof(int));
-	digits[0] = 0;
-	digits[1] = 1;
-	digits[2] = 2;
-	// int digits[] = {9, 9, 9};
-	int digitsSize = 3;
-	int returnSize = 20;
-	// int *p = plusOne(digits, digitsSize, &returnSize);
-	int *p = (int *)realloc(digits, 20 * sizeof(int));
-	for (int i = 0; i < returnSize; ++i) printf("%d\n", p[i]);
+	char s[] = "()[]{";
+	printf("%d\n", isValid(s));
 
 	return 0;
 }

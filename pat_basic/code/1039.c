67813a9dd4ef2035976700d509101b6eec817c66
cxy <37500268+chenxygh@users.noreply.github.com>
2019-04-06 22:05:49 +0800

    PAT (Basic Level) Practice: 1039 到底买不买 - undone

---
+++ pat_basic/code/1039.c
@@ -0,0 +1,25 @@
+/*
+* @Author: cxy
+* @Date:   2019-04-06 22:01:45
+* @Last Modified by:   cxy
+* @Last Modified time: 2019-04-06 22:03:58
+*/
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+	char src[1001] = {'\0'};
+	char target[1001] = {'\0'};
+	scanf("%[^\n]s", src);
+	getchar();
+	scanf("%[^\n]s", target);
+
+	char *p = src;
+	char *q = target;
+	while (p[0] != '\0' && q != '\0') {
+		
+	}
+
+	return 0;
+}

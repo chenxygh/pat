11f3425e2ad6957feac36a7ad36eff84b7734ca7
cxy <37500268+chenxygh@users.noreply.github.com>
2019-04-08 21:53:37 +0800

    PAT (Basic Level) Practice: 1054 求平均值 - undone

---
+++ pat_basic/code/1054.c
@@ -0,0 +1,22 @@
+/*
+* @Author: cxy
+* @Date:   2019-04-08 21:50:21
+* @Last Modified by:   cxy
+* @Last Modified time: 2019-04-08 21:52:49
+*/
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+	int cnt = 0;
+	scanf("%d", &cnt);
+
+	while (cnt--) {
+		char str[10] = {"\0"};
+		scanf("%s", str);
+		getchar();
+	}
+
+	return 0;
+}

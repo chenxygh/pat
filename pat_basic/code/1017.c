6a2fbd6e537614b64bb23442724673460d64fcb9
cxy <37500268+chenxygh@users.noreply.github.com>
2019-04-09 17:37:08 +0800

    PAT (Basic Level) Practice: 1017 A除以B - modified 优化代码量

--- pat_basic/code/1017.c
+++ pat_basic/code/1017.c
@@ -2,7 +2,7 @@
 * @Author: cxy
 * @Date:   2019-03-31 23:04:19
 * @Last Modified by:   cxy
-* @Last Modified time: 2019-03-31 23:37:41
+* @Last Modified time: 2019-04-09 17:35:22
 */
 
 #include <stdio.h>
@@ -19,18 +19,13 @@ int main(int argc, char const *argv[])
 	for (int i = 0; digits[i] != '\0'; ++i) {
 		int divident = remainder * 10 + digits[i] - '0';
 		int temp = divident / divisor;
-		quotient[length++] = temp? temp + '0': '0';
+		quotient[length++] = temp + '0';
 		remainder = divident % divisor;
 	}
 
 	int i = 0;
 	for (; quotient[i] == '0' && i < length; ++i);
-	if (i == length) {
-		printf("%d %d\n", 0, remainder);
-		return 0;
-	}
-
-	printf("%s %d\n", quotient + i, remainder);
+	i == length? printf("0 %d\n", remainder): printf("%s %d\n", quotient + i, remainder);
 
 	return 0;
 }

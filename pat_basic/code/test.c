94681a83780ba3b9c891adf372a0d9ebbb5e2634
cxy <37500268+chenxygh@users.noreply.github.com>
2019-04-16 21:53:47 +0800

    common commit

--- pat_basic/code/test.c
+++ pat_basic/code/test.c
@@ -2,27 +2,47 @@
 * @Author: cxy
 * @Date:   2019-04-01 01:01:12
 * @Last Modified by:   cxy
-* @Last Modified time: 2019-04-15 20:46:06
+* @Last Modified time: 2019-04-15 23:07:50
 */
 
 #include <stdio.h>
 
-struct stu {
-	int score;
-	int age;
-	int id[10];
-};
+int isLeapYear (int year) {
+	return !(year % 400) || (!(year % 4) && year % 100);
+}
 
 int main(int argc, char const *argv[])
 {
-	struct stu temp = {
-		.score = 10,
-		.age = 11,
-		.id = {22}
-	};
-	printf("%d\n", temp.score);
-	printf("%d\n", temp.age);
-	printf("%d\n", temp.id[0]);
+	const int month[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+	int dateA = 0;
+	int dateB = 0;
+	while (scanf("%d%d", &dateA, &dateB) != EOF) {
+		int operandA = dateA > dateB? dateA: dateB;
+		int operandB = dateA < dateB? dateA: dateB;
+
+		int yearA = operandA / 10000;
+		int yearB = operandB / 10000;
+		int days = 0;
+		for (int i = yearB; yearA >= i; ++i) days += isLeapYear(i)? 366: 365;
+
+		int monthA = operandA / 100 % 100;
+		int monthB = operandB / 100 % 100;
+		for (int i = 1; i < monthB; ++i) {
+			days -= month[i - 1];
+			if (i == 2 && isLeapYear(yearB)) --days;
+		}
+		for (int i = 12; i > monthA; --i) {
+			days -= month[i - 1];
+			if (i == 2 && isLeapYear(yearA)) --days;
+		}
+
+		int temp = monthA == 2 && isLeapYear(yearA)? 29: month[monthA - 1];
+		days = days - (operandB % 100 - 1);
+		days = days - (temp - operandA % 100);
+
+		printf("%d\n", days);
+	}
 
 	return 0;
 }

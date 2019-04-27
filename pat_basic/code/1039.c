09f6740d9fe9d77c3d8a52bf64489a5a33355e1f
cxy <37500268+chenxygh@users.noreply.github.com>
2019-04-27 20:17:20 +0800

    PAT (Basic Level) Practice: 1039 到底买不买 (数组下标法，类似 1033)

--- pat_basic/code/1039.c
+++ pat_basic/code/1039.c
@@ -2,24 +2,34 @@
 * @Author: cxy
 * @Date:   2019-04-06 22:01:45
 * @Last Modified by:   cxy
-* @Last Modified time: 2019-04-06 22:03:58
+* @Last Modified time: 2019-04-27 20:13:37
 */
 
 #include <stdio.h>
 
 int main(int argc, char const *argv[])
 {
-	char src[1001] = {'\0'};
-	char target[1001] = {'\0'};
-	scanf("%[^\n]s", src);
-	getchar();
-	scanf("%[^\n]s", target);
+	int ASCII[256] = {0};
 
-	char *p = src;
-	char *q = target;
-	while (p[0] != '\0' && q != '\0') {
-		
+	char temp = 0;
+	int total = 0;
+	while ((temp = getchar()) != '\n') {
+		++ASCII[temp];
+		++total;
 	}
+
+
+	int deficient = 0;
+	while ((temp = getchar()) != '\n') {
+		if (ASCII[temp]) {
+			--ASCII[temp];
+			--total;
+		} else {
+			++deficient;
+		}
+	}
+
+	deficient? printf("No %d\n", deficient): printf("Yes %d\n", total);
 
 	return 0;
 }

670537d153b553de197c128079e7a848ed18df7f
cxy <37500268+chenxygh@users.noreply.github.com>
2019-05-07 17:28:05 +0800

    PAT (Basic Level) Practice: 1067-undone

--- pat_basic/code/1067-undone.c
+++ pat_basic/code/1067-undone.c
@@ -2,7 +2,7 @@
 * @Author: cxy
 * @Date:   2019-04-26 21:36:47
 * @Last Modified by:   cxy
-* @Last Modified time: 2019-04-26 22:56:56
+* @Last Modified time: 2019-05-06 13:29:26
 */
 
 #include <stdio.h>
@@ -10,48 +10,29 @@
 
 int main(int argc, char const *argv[])
 {
-	char str[21] = {'\0'};
+	char str[100] = {'\0'};
 	int cnt = 0;
 	scanf("%s%d", str, &cnt);
-	int strSize = strlen(str);
 
 	int wrongCount = 0;
-	char typed[cnt][21] = {'\0'};
-	char isCorrect = 0;
-	char isOverflow = 0;
-	int i = 0;
-	for (; 1; ++i) {
-		char temp[21] = {'\0'};
-		char *p = temp;
-		if (!isCorrect && !isOverflow) {
-			if (i >= cnt) {
-				isOverflow = 1;
-			} else {
-				p = typed[i];
-			}
-			// 三元运算符似乎在不同类型的赋值下，会有错误
-			// i >= cnt? isOverflow = 1: p = typed[i];
-		}
+	while (1) {
+		char temp[100] = {'\0'};
 		getchar();
-		scanf("%[^\n]s", p);
-		if (!strcmp("#", p)) break;
-		if (!isCorrect && !isOverflow && !strcmp(str, p)) {
-			isCorrect = 1;
+		scanf("%[^\n]s", temp);
+		if (temp[0] == '#') {
+			break;
 		}
-	}
 
-	if (isCorrect) {
-		for (int j = 0; typed[j + 1][0] != '\0'; ++j) {
-			printf("Wrong password: %s\n", typed[j]);
+		if (!strcmp(temp, str)) {
+			printf("Welcome in\n");
+			break;
 		}
-		printf("Welcome in\n");
-	} else {
-		int j = 0;
-		for (; j < cnt && typed[j][0] != '\0'; ++j) {
-			printf("Wrong password: %s\n", typed[j]);
-		}
-		if (i > cnt) {
+
+		printf("Wrong password: %s\n", temp);
+		++wrongCount;
+		if (wrongCount >= cnt) {
 			printf("Account locked\n");
+			break;
 		}
 	}
 

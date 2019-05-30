b8d4cd71dc08c3e02bca2266e9e9342b6205522a
cxy <37500268+chenxygh@users.noreply.github.com>
2019-05-30 11:42:27 +0800

    PAT (Basic Level) Practice: 1058 选择题

--- pat_basic/code/1058.c
+++ pat_basic/code/1058.c
@@ -2,7 +2,7 @@
 * @Author: cxy
 * @Date:   2019-05-27 12:49:45
 * @Last Modified by:   cxy
-* @Last Modified time: 2019-05-27 16:18:09
+* @Last Modified time: 2019-05-30 11:38:43
 */
 
 #include <stdio.h>
@@ -35,6 +35,8 @@ int main(int argc, char const *argv[])
 			getchar();
 			scanf("%c", answer[i].items + j);
 		}
+
+		// printf("%d %d %d %s\n", answer[i].score, answer[i].itemsTotal, answer[i].itemsValidTotal, answer[i].items);
 	}
 
 	int wrong[questionTotal] = {0};
@@ -47,18 +49,18 @@ int main(int argc, char const *argv[])
 			scanf("%d", &num);
 
 			bool isValid = true;
-			for (int k = 0; k < num; ++j) {
+			for (int k = 0; k < num; ++k) {
 				getchar();// 吸收空格符
 				char temp = getchar();
 
-				if (isValid && (num != answer[i].itemsValidTotal || !includes(answer[i].items, temp))) {
+				if (isValid && (num != answer[j].itemsValidTotal || !includes(answer[j].items, temp))) {
 					isValid = !isValid;
 				}
 			}
 			getchar();// 吸收')'
 
 			if (isValid) {
-				score += answer[i].score;
+				score += answer[j].score;
 			} else {
 				++wrong[j];
 			}
@@ -70,11 +72,16 @@ int main(int argc, char const *argv[])
 	// 统计结果输出
 	int max = 0;
 	for (int i = 0; i < questionTotal; ++i) {
-		if (max > wrong[i]) max = wrong[i];
+		if (max < wrong[i]) max = wrong[i];
 	}
-	for (int i = 0; i < questionTotal; ++i) {
-		if (wrong[i] == max) {
-			printf("%d %d\n", max, i + 1);
+	if (!max) {
+		printf("Too simple\n");
+	} else {
+		printf("%d", max);
+		for (int i = 0; i < questionTotal; ++i) {
+			if (wrong[i] == max) {
+				printf(" %d", i + 1);
+			}
 		}
 	}
 

e69c543d358667bcd1ca8b41064865b7187701f9
cxy <37500268+chenxygh@users.noreply.github.com>
2020-01-17 15:27:06 +0800

    add gitignore

---
+++ pat_basic/code/1005.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+	int dict[151] = {0};
+	int n = 0;
+	scanf("%d", &n);
+	printf("%d\n", 10);
+	// while (n--) {
+	// 	int num = 0;
+	// 	scanf("%d", &num);
+	// 	printf("--: %d\n", num);
+	// }
+
+	return 0;
+}
